48e9016d656f2df56e66cfc9dd40e47b0fcf1bfc
Anthony Walker <awalk91@hotmail.com>
2014-06-17 21:46:56 -0500

    addition of shuffle method and semi-working string reader

--- gibgen.java
+++ gibgen.java
@@ -2,11 +2,42 @@
  * Anthony Walker
  * SWEAP Code Challenge: Gibberish Generator
  * June 22, 2014
- *
  */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Random;
 
- public class gibgen{
-	public static void main(String[] args){
-		//TODO
-	}
+public class gibgen{
+    public static void main(String[] args){
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String temp = null; 
+        
+        //for (String s: args) System.out.println(s);
+        //System.exit(0);
+        
+        if (args.length == 0){
+            System.out.println("Error: you must enter input");
+        }
+        
+        else{
+            //try{
+                temp = args[0];
+            //}
+            //catch(IOException e){System.err.print(e+"");}
+        }
+        
+        System.out.println(temp);
+    }
+    
+    public static String shuff(String string){
+        String[] separateWords = string.split(" ");
+        String temp = null;
+        
+        Random random = new Random();
+        for (int i = 0; i < separateWords.length; i++){
+            System.out.println(separateWords[i]);
+        }
+        return "finish";
+    }
  }
